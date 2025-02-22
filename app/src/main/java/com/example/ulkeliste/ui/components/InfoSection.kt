b27f6c2b1f2fcaecdbb428c753648519da3f5e89
Necdet <117301669+AhplaXx@users.noreply.github.com>
2025-02-23 01:00:18 +0300

    Detail Page Updated

---
+++ app/src/main/java/com/example/ulkeliste/ui/components/InfoSection.kt
@@ -0,0 +1,71 @@
+package com.example.ulkeliste.ui.components
+
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun InfoSection(
+    icon: Int,
+    label:String,
+    text:String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+
+            containerColor = Color(0xFF2973B2),
+            contentColor = Color(0xFFF2EFE7)
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp),
+
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 14.dp)
+        ) {
+            Image(
+                painter = painterResource(id = icon),
+                contentScale = ContentScale.Fit,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+                    .size(32.dp)
+            )
+            Column {
+                Text(text = label, fontWeight = FontWeight.Bold, color = Color(0xFFF2EFE7))
+                Spacer(modifier = Modifier.height(3.dp))
+                Text(text = text)
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/ulkeliste/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/ulkeliste/ui/screens/DetailScreen.kt
@@ -1,15 +1,20 @@
 package com.example.ulkeliste.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.ulkeliste.R
 import com.example.ulkeliste.model.CountryModel
 import com.example.ulkeliste.ui.CountryViewModel
+import com.example.ulkeliste.ui.components.InfoSection
 
 
 @Composable
@@ -22,9 +27,17 @@ fun DetailScreen(navController: NavController){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("DETAIL SCREEN")
+        //Flag Image
+        Box(
+            modifier = Modifier){
+        }
+
         country?.let {
-            Text(text = it.name?.common ?: "Bilinmiyor")
+            InfoSection(
+                modifier = Modifier.padding(8.dp),
+                icon = R.drawable.worldd_removebg_preview, label ="Country Name" , text =it.name?.common ?:"Unknown"
+            )
+            InfoSection(icon = R.drawable.worldd_removebg_preview, label ="UN Member" , text = it.unMember.toString())
         }
 
 
